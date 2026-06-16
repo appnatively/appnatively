f0ca5915bc0d17054b97995457c3f7b9892110c9
Tanjim <34968601+tanjimhasan@users.noreply.github.com>
2026-06-16 12:15:18 +0600

    Update WPFormsTest.php

--- tests/Integrations/WPFormsTest.php
+++ tests/Integrations/WPFormsTest.php
@@ -26,156 +26,59 @@ class WPFormsTest extends \WP_UnitTestCase {
         parent::setUp();
 
         $form_data = [
-            'field_id' => '20',
+            'field_id' => 10,
             'fields'   => [
-                '0' => [
-                    'id'       => '0',
-                    'type'     => 'text',
-                    'label'    => 'Full Name',
-                    'required' => '1',
-                    'size'     => 'medium',
-                ],
                 '1' => [
-                    'id'       => '1',
-                    'type'     => 'text',
-                    'label'    => 'Full Name 2',
-                    'required' => '0',
-                    'size'     => 'medium',
-                ],
-                '2' => [
-                    'id'       => '2',
-                    'type'     => 'number',
-                    'label'    => 'Number',
-                    'required' => '1',
-                    'min'      => '1',
-                    'max'      => '100',
-                ],
-                '3' => [
-                    'id'       => '3',
-                    'type'     => 'number',
-                    'label'    => 'Number 2',
-                    'required' => '0',
-                ],
-                '4' => [
-                    'id'       => '4',
-                    'type'     => 'email',
-                    'label'    => 'Email',
-                    'required' => '1',
-                ],
-                '5' => [
-                    'id'       => '5',
-                    'type'     => 'email',
-                    'label'    => 'Email 2',
-                    'required' => '0',
-                ],
-                '6' => [
-                    'id'       => '6',
-                    'type'     => 'url',
-                    'label'    => 'Website',
-                    'required' => '1',
+                    'id'          => '1',
+                    'type'        => 'text',
+                    'label'       => 'Single Line Text',
+                    'size'        => 'medium',
+                    'limit_count' => '1',
+                    'limit_mode'  => 'characters',
                 ],
                 '7' => [
                     'id'       => '7',
-                    'type'     => 'url',
-                    'label'    => 'Website 2',
-                    'required' => '0',
-                ],
-                '8' => [
-                    'id'       => '8',
-                    'type'     => 'checkbox',
-                    'label'    => 'Checkboxes',
+                    'type'     => 'email',
+                    'label'    => 'Email',
                     'required' => '1',
-                    'choices'  => [
-                        '1' => [ 'label' => 'Choice 1', 'value' => '' ],
-                        '2' => [ 'label' => 'Choice 2', 'value' => '' ],
+                    'size'     => 'medium',
+                ],
+                '2' => [
+                    'id'      => '2',
+                    'type'    => 'select',
+                    'label'   => 'Dropdown',
+                    'choices' => [
+                        '1' => [ 'label' => 'First Choice', 'value' => '' ],
+                        '2' => [ 'label' => 'Second Choice', 'value' => '' ],
+                        '3' => [ 'label' => 'Third Choice', 'value' => '' ],
                     ],
                 ],
-                '9' => [
-                    'id'       => '9',
-                    'type'     => 'checkbox',
-                    'label'    => 'Checkboxes 2',
-                    'required' => '0',
-                    'choices'  => [
-                        '1' => [ 'label' => 'Choice 1', 'value' => '' ],
+                '4' => [
+                    'id'      => '4',
+                    'type'    => 'checkbox',
+                    'label'   => 'Checkboxes',
+                    'choices' => [
+                        '1' => [ 'label' => 'First Choice', 'value' => '' ],
+                        '2' => [ 'label' => 'Second Choice', 'value' => '' ],
+                        '3' => [ 'label' => 'Third Choice', 'value' => '' ],
                     ],
                 ],
-                '10' => [
-                    'id'      => '10',
-                    'type'     => 'select',
-                    'label'    => 'Dropdown',
-                    'required' => '1',
-                    'choices'  => [
-                        '1' => [ 'label' => 'Option 1', 'value' => '' ],
-                        '2' => [ 'label' => 'Option 2', 'value' => '' ],
-                        '3' => [ 'label' => 'Option 3', 'value' => '' ],
-                    ],
+                '5' => [
+                    'id'    => '5',
+                    'type'  => 'number',
+                    'label' => 'Numbers',
+                    'size'  => 'medium',
                 ],
-                '11' => [
-                    'id'      => '11',
-                    'type'     => 'select',
-                    'label'    => 'Dropdown 2',
-                    'required' => '0',
-                    'choices'  => [
-                        '1' => [ 'label' => 'Option 1', 'value' => '' ],
-                        '2' => [ 'label' => 'Option 2', 'value' => '' ],
-                        '3' => [ 'label' => 'Option 3', 'value' => '' ],
-                    ],
-                ],
-                '12' => [
-                    'id'       => '12',
-                    'type'     => 'number-slider',
-                    'label'    => 'Number Slider',
-                    'required' => '1',
-                    'min'      => '0',
-                    'max'      => '100',
-                ],
-                '13' => [
-                    'id'       => '13',
-                    'type'     => 'number-slider',
-                    'label'    => 'Number Slider 2',
-                    'required' => '0',
-                ],
-                '14' => [
-                    'id'         => '14',
-                    'type'       => 'rating',
-                    'label'      => 'Rating',
-                    'required'   => '1',
-                    'max_rating' => '5',
-                ],
-                '15' => [
-                    'id'       => '15',
-                    'type'     => 'rating',
-                    'label'    => 'Rating 2',
-                    'required' => '0',
-                ],
-                '16' => [
-                    'id'       => '16',
-                    'type'     => 'date-time',
-                    'label'    => 'Date Time',
-                    'required' => '1',
-                ],
-                '17' => [
-                    'id'       => '17',
-                    'type'     => 'date-time',
-                    'label'    => 'Date Time 2',
-                    'required' => '0',
-                ],
-                '18' => [
-                    'id'       => '18',
-                    'type'     => 'password',
-                    'label'    => 'Password',
-                    'required' => '1',
-                ],
-                '19' => [
-                    'id'       => '19',
-                    'type'     => 'password',
-                    'label'    => 'Password 2',
-                    'required' => '0',
-                ],
-                '20' => [
-                    'id'    => '20',
-                    'type'  => 'internal-information',
-                    'label' => 'Internal Info',
+                '6' => [
+                    'id'            => '6',
+                    'type'          => 'number-slider',
+                    'label'         => 'Number Slider',
+                    'min'           => '10',
+                    'max'           => '24',
+                    'default_value' => '24',
+                    'step'          => '1',
+                    'value_display' => 'Selected Value: {value}',
+                    'size'          => 'medium',
                 ],
             ],
             'settings' => [
@@ -245,7 +148,7 @@ class WPFormsTest extends \WP_UnitTestCase {
         $this->assertNotEmpty( $form );
         $this->assertEquals( $this->form_id, $form['id'] );
         $this->assertNotEmpty( $form['fields'] );
-        $this->assertCount( 21, $form['fields'] );
+        $this->assertCount( 6, $form['fields'] );
     }
 
     public function test_get_validation_rules() {
@@ -254,52 +157,23 @@ class WPFormsTest extends \WP_UnitTestCase {
         $rules   = $wpforms->expose_get_validation_rules( $form );
 
         $expected_fields = [
-            'full_name',
-            'full_name_2',
-            'number',
-            'number_2',
+            'single_line_text',
             'email',
-            'email_2',
-            'website',
-            'website_2',
-            'checkboxes',
-            'checkboxes_2',
             'dropdown',
-            'dropdown_2',
+            'checkboxes',
+            'numbers',
             'number_slider',
-            'number_slider_2',
-            'rating',
-            'rating_2',
-            'date_time',
-            'date_time_2',
-            'password',
-            'password_2',
         ];
         foreach ( $expected_fields as $field ) {
             $this->assertArrayHasKey( $field, $rules );
         }
-        $this->assertArrayNotHasKey( 'internal_info', $rules );
 
-        $this->assertEquals( 'string|required', $rules['full_name'] );
-        $this->assertEquals( 'string', $rules['full_name_2'] );
-        $this->assertEquals( 'numeric|min:1|max:100|required', $rules['number'] );
-        $this->assertEquals( 'numeric', $rules['number_2'] );
+        $this->assertEquals( 'string', $rules['single_line_text'] );
         $this->assertEquals( 'string|email|required', $rules['email'] );
-        $this->assertEquals( 'string|email', $rules['email_2'] );
-        $this->assertEquals( 'string|url|required', $rules['website'] );
-        $this->assertEquals( 'string|url', $rules['website_2'] );
-        $this->assertEquals( 'array|required', $rules['checkboxes'] );
-        $this->assertEquals( 'array', $rules['checkboxes_2'] );
-        $this->assertEquals( 'string|in:Option 1,Option 2,Option 3|required', $rules['dropdown'] );
-        $this->assertEquals( 'string|in:Option 1,Option 2,Option 3', $rules['dropdown_2'] );
-        $this->assertEquals( 'numeric|min:0|max:100|required', $rules['number_slider'] );
-        $this->assertEquals( 'numeric', $rules['number_slider_2'] );
-        $this->assertEquals( 'integer|max:5|required', $rules['rating'] );
-        $this->assertEquals( 'integer', $rules['rating_2'] );
-        $this->assertEquals( 'string|required', $rules['date_time'] );
-        $this->assertEquals( 'string', $rules['date_time_2'] );
-        $this->assertEquals( 'string|required', $rules['password'] );
-        $this->assertEquals( 'string', $rules['password_2'] );
+        $this->assertEquals( 'string|max:255', $rules['dropdown'] );
+        $this->assertEquals( 'array', $rules['checkboxes'] );
+        $this->assertEquals( 'numeric', $rules['numbers'] );
+        $this->assertEquals( 'numeric|min:10|max:24', $rules['number_slider'] );
     }
 
     public function test_submit() {
@@ -309,16 +183,12 @@ class WPFormsTest extends \WP_UnitTestCase {
         $wp_request = new \WP_REST_Request();
         $wp_request->set_param( 'form_id', $this->form_id );
         $wp_request->set_param( 'integration', 'wpforms' );
-        $wp_request->set_param( 'full_name', 'John' );
-        $wp_request->set_param( 'number', '42' );
-        $wp_request->set_param( 'email', 'john@example.com' );
-        $wp_request->set_param( 'website', 'https://example.com' );
-        $wp_request->set_param( 'checkboxes', [ 'Choice 1' ] );
-        $wp_request->set_param( 'dropdown', 'Option 1' );
-        $wp_request->set_param( 'number_slider', '75' );
-        $wp_request->set_param( 'rating', '5' );
-        $wp_request->set_param( 'date_time', '2024-01-15' );
-        $wp_request->set_param( 'password', 'secret123' );
+        $wp_request->set_param( 'single_line_text', 'Hello' );
+        $wp_request->set_param( 'email', 'test@example.com' );
+        $wp_request->set_param( 'dropdown', 'First Choice' );
+        $wp_request->set_param( 'checkboxes', [ 'First Choice' ] );
+        $wp_request->set_param( 'numbers', '42' );
+        $wp_request->set_param( 'number_slider', '20' );
         $wp_request->set_param( 'unsupported', 'should be skipped' );
 
         $request = new Request( $wp_request );
@@ -337,21 +207,17 @@ class WPFormsTest extends \WP_UnitTestCase {
 
         $process = wpforms()->obj( 'process' );
         $this->assertEmpty( $process->errors, 'WPForms validation errors: ' . print_r( $process->errors, true ) );
-        $this->assertNotEmpty( $process->entry_id, 'WPForms did not create an entry' );
+        // WPForms Lite does not persist entries, so entry_id is always 0.
         $this->assertEquals( $this->form_id, $captured_entry['id'] );
         $this->assertArrayHasKey( 'fields', $captured_entry );
 
         $fields = $captured_entry['fields'];
-        $this->assertEquals( 'John', $fields['0'] );
-        $this->assertEquals( '42', $fields['2'] );
-        $this->assertEquals( 'john@example.com', $fields['4'] );
-        $this->assertEquals( 'https://example.com', $fields['6'] );
-        $this->assertEquals( [ 'Choice 1' ], $fields['8'] );
-        $this->assertEquals( 'Option 1', $fields['10'] );
-        $this->assertEquals( '75', $fields['12'] );
-        $this->assertEquals( '5', $fields['14'] );
-        $this->assertEquals( '2024-01-15', $fields['16'] );
-        $this->assertEquals( 'secret123', $fields['18'] );
-        $this->assertArrayNotHasKey( '20', $fields );
+        $this->assertEquals( 'Hello', $fields['1'] );
+        $this->assertEquals( 'test@example.com', $fields['7'] );
+        $this->assertEquals( 'First Choice', $fields['2'] );
+        $this->assertEquals( [ 'First Choice' ], $fields['4'] );
+        $this->assertEquals( '42', $fields['5'] );
+        $this->assertEquals( '20', $fields['6'] );
+        $this->assertArrayNotHasKey( '10', $fields );
     }
 }
